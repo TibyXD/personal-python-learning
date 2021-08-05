165a4e0285c78245957fe7a4a65bcd111b6dcb2d
TibyXD <tibyxd@gmail.com>
2021-08-05 20:17:05 +0300

    Update prob1

--- Fp1/lab2/prob1.py
+++ Fp1/lab2/prob1.py
@@ -1,23 +1,23 @@
-given_number = int(input())
+GivenNum = int(input())
 
 
-def check_prime_number(checked_number):
-    if checked_number < 2:
+def check_prime_number(CheckedNum):
+    if CheckedNum < 2:
         return False
-    if checked_number == 2:
+    if CheckedNum == 2:
         return True
-    if checked_number % 2 == 0:
+    if CheckedNum % 2 == 0:
         return False
 
-    for x in range(3, int(checked_number / 2), 2):
-        if checked_number % x == 0:
+    for x in range(3, int(CheckedNum / 2), 2):
+        if CheckedNum % x == 0:
             return False
 
     return True
 
 
-def first_prime_after_given(reff_number):
-    x = reff_number + 1
+def first_prime_after_given(ReffNum):
+    x = ReffNum + 1
     while True:
         if check_prime_number(x) == True:
             break
@@ -27,4 +27,4 @@ def first_prime_after_given(reff_number):
     return x
 
 
-print(first_prime_after_given(given_number))
+print(first_prime_after_given(GivenNum))
